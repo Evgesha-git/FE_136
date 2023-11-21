6b913b9e3ce83bed3087111559034e4c530f6043
Evgenii <chico7496@gmail.com>
2023-11-21 21:44:17 +0300

    objects

--- scripts/main.js
+++ scripts/main.js
@@ -1,92 +1,175 @@
-// @ts-check
-/**
- *
- * @param {number} min
- * @param {number} max
- */
-function getRandomNum(min = 0, max = 0) {
-    return Math.floor(Math.random() * (max - min) + min);
-}
+let date = "2026-11-27 В переменной date лежит дата в формате";
 
-/**
- *
- * @param {number} min
- * @param {number} max
- * @param {number[]} ex
- */
-function getRnEx(min, max, ex) {
-    let num = getRandomNum(min, max);
-    if (ex.includes(num)) return getRnEx(min, max, ex);
-    return num;
-}
+const regDate = /(\d{4})-(\d{2})-(\d{2})/g;
+// date = date.split('-').reverse().join('/');
+date = date.replace(regDate, "$3/$2/$1");
+console.log(date);
 
-console.log(getRnEx(1, 10, [1, 2, 3, 5, 7, 9]));
-
-/**
- *
- * @param {string} str
- */
-function getStr(str) {
-    let regExp = /ab+a/g;
-    let rez = str.match(regExp);
-    return rez;
-}
-
-console.log(getStr("aa aba abba abbba abca abea"));
-
-/**
- * trim()
- * trimStart()
- * trimEnd()
- */
-
-let s = "\t\n             sdf sdf sdghfjsdf    \n \t ";
-console.log(s);
-console.log(s.trim());
-console.log(s.trimEnd());
-console.log(s.trimStart());
-
-/**
- * @type {string}
- */
-let s2 = "translateX(100%)";
-s2 = s2.replace(/\D/g, ""); // [^0-9]
-// s2 = parseFloat(s2);
-console.log(s2);
-
-/**
- *
- * @param {string} mail
- * @returns boolean
- */
-const email = (mail) => {
-    const regExp =
-        /^[a-z]{1}[a-z0-9_\.\-]{1,}@[a-z]{1}[a-z0-9_\.\-]{1,10}\.[a-z]{2,11}$/gi;
-    return regExp.test(mail);
+const user = {
+    name: "Alex",
+    nickName: "Alexx",
+    user_id: 12532,
+    date: "12-20-1999",
 };
 
-console.log(email("ящик@почта.бел"));
-console.log(email("user_top@mail.com"));
-console.log(email("1user1@mail.ru"));
-console.log(email("t1@m.r"));
-console.log(email("t!-1@mail.gg"));
+function User(name, nickName, date) {
+    let self = this;
+    this.name = name;
+    this.nickName = nickName;
+    this.date = date;
+    // let age = 0;
+    let getId = function () {
+        return Math.floor(Math.random() * 10000);
+    };
+    this.user_id = getId();
 
-/**
- * @param {string} url
- */
-const adress = (url) => {
-    const regExp =
-        /(https?:\/\/[a-z][a-z0-9]+(?:\.[a-z0-9]+)*\.[a-z]{2,11})(\/(?:[^#\?\s])+)?(\?[^#]+)?(#\w+)?/gi;
-    let group = regExp.exec(url) || [];
+    this.chageName = function (name) {
+        this.name = name;
+    }
 
-    return [...group].filter((item, index) => (index !== 0 ? item : null));
+    this.getInfo = function () {
+        return getFullInfo();
+    }
+
+    // function getFullInfo (){
+    //     return `${self.name}, ${self.nickName}, ${self.date}`;
+    // }
+
+    const getFullInfo = () => {
+        return `${this.name}, ${this.nickName}, ${this.date}`;
+    }
+
+    // this.getAge = () => {
+    //     return age;
+    // }
+
+    // this.setAge = (num) => {
+    //     if (num > 0 && num < 120) {
+    //         age = num
+    //     } else {
+    //         throw new Error('Неверное значение возраста');
+    //     }
+    // }
+
+    // this.toString = () => {
+    //     return `${this.name}, ${this.nickName}, ${this.getAge()}`;
+    // }
+
+    Object.defineProperty(this, 'age', {
+        enumerable: true,
+        configurable: true,
+        get: function () {
+            return age + ' лет';
+        },
+        set: function (num) {
+            if (num > 1 && num < 120){
+                age = num;
+            } else {
+                throw new TypeError('Неверное значение');
+            }
+        }
+    })
+
+    User.counter++;
+}
+
+User.prototype.toString = function() {
+    return `${this.name}, ${this.nickName}, ${this.age}`;
+}
+
+User.counter = 0;
+User.getCount = function () {
+    return User.counter;
+}
+
+Array.prototype.hello = () => 'Hello World';
+
+const user2 = new User("Petr", "petro", "2001-09-12");
+console.log(user2);
+
+Object.defineProperty(user, 'age', {
+    enumerable: true,
+    configurable: true,
+    // writable: true, // без get/set
+    // value: 0, // без get/set
+    get: function () {
+        return age + 'лет'
+    },
+    set: function (a) {
+        if (a > 0 && a < 120) {
+            age = a;
+        } else {
+            throw new Error('Неверное значение');
+        }
+    },
+});
+
+function Contact (name, age, phone, email) {
+    this.name = name;
+    this.age = age;
+    this.phone = phone;
+    this.email = email;
+
+    this.getInfo = () => {
+        return `Name: ${this.name}, Age: ${this.age}, Phone: ${this.phone}, Email: ${this.email}`;
+    }
+
+    this.toString = () => {
+        return `Name: ${this.name}. Age: ${this.age}. Phone: ${this.phone}. Email: ${this.email}. Id: ${this.id}.`
+    }
+}
+
+function Contacts () {
+    this.contacts = [];
+    let id = 0;
+
     /**
-     * function(item, index) {
-     *  if (index !== 0) {
-     *      return item;
-     *  } else {
-     *      return null;
-     *  }
-     * }
+     * @param {string} name 
+     * @param {number} age 
+     * @param {string} phone 
+     * @param {string} email 
      */
-};
+    this.add = (name, age, phone, email) => {
+        if (age < 0 || age > 120) return;
+        if (name.length === 0) return;
+        if (phone.length === 0) return;
+        if (email.length === 0) return;
+
+        let contact = new Contact(name, age, phone, email);
+        contact.id = id;
+        id++;
+        this.contacts.push(contact);
+    }
+
+    this.getContacts = () => {
+        return this.contacts;
+    }
+
+    this.getContact = (id) => {
+        let contact = this.contacts.find((item) => item.id === id);
+        return contact.getInfo();
+    }
+
+    this.removeContact = (id) => {
+        this.contacts = this.contacts.filter((item) => item.id !== id);
+    }
+}
+
+const contactNote = new Contacts();
+contactNote.add('Alex1', 22, '236452374', 'sdgfjds@mail.com');
+contactNote.add('Alex2', 12, '236452374', 'sdgfjds@mail.com');
+contactNote.add('Alex3', 32, '236452374', 'sdgfjds@mail.com');
+contactNote.add('Alex4', 42, '236452374', 'sdgfjds@mail.com');
+contactNote.add('Alex5', 52, '236452374', 'sdgfjds@mail.com');
+contactNote.add('Alex6', 62, '236452374', 'sdgfjds@mail.com');
+contactNote.add('Alex7', 72, '236452374', 'sdgfjds@mail.com');
+
+/**
+ * map
+ * find
+ * filter
+ * reduce
+ * forEach
+ * every
+ * some
+ */
