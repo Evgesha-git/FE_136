647ddd4812e2e18bea95da6ed83ae3d80c3084fe
Evgenii <chico7496@gmail.com>
2023-12-19 21:32:02 +0300

    extends

--- scripts/main.js
+++ scripts/main.js
@@ -1,168 +1,157 @@
-let date = "2026-11-27 В переменной date лежит дата в формате";
-
-const regDate = /(\d{4})-(\d{2})-(\d{2})/g;
-// date = date.split('-').reverse().join('/');
-date = date.replace(regDate, "$3/$2/$1");
-console.log(date);
-
-const user = {
-    name: "Alex",
-    nickName: "Alexx",
-    user_id: 12532,
-    date: "12-20-1999",
-};
-
-function User(name, nickName, date) {
-    let self = this;
-    this.name = name;
-    this.nickName = nickName;
-    this.date = date;
-    // let age = 0;
-    let getId = function () {
-        return Math.floor(Math.random() * 10000);
-    };
-    this.user_id = getId();
-
-    this.chageName = function (name) {
-        this.name = name;
-    }
-
-    this.getInfo = function () {
-        return getFullInfo();
-    }
-
-    // function getFullInfo (){
-    //     return `${self.name}, ${self.nickName}, ${self.date}`;
-    // }
-
-    const getFullInfo = () => {
-        return `${this.name}, ${this.nickName}, ${this.date}`;
-    }
-
-    // this.getAge = () => {
-    //     return age;
-    // }
-
-    // this.setAge = (num) => {
-    //     if (num > 0 && num < 120) {
-    //         age = num
-    //     } else {
-    //         throw new Error('Неверное значение возраста');
-    //     }
-    // }
-
-    // this.toString = () => {
-    //     return `${this.name}, ${this.nickName}, ${this.getAge()}`;
-    // }
-
-    Object.defineProperty(this, 'age', {
-        enumerable: true,
-        configurable: true,
-        get: function () {
-            return age + ' лет';
-        },
-        set: function (num) {
-            if (num > 1 && num < 120){
-                age = num;
-            } else {
-                throw new TypeError('Неверное значение');
-            }
-        }
-    })
-
-    User.counter++;
-}
-
-User.prototype.toString = function() {
-    return `${this.name}, ${this.nickName}, ${this.age}`;
-}
-
-User.counter = 0;
-User.getCount = function () {
-    return User.counter;
-}
-
-Array.prototype.hello = () => 'Hello World';
-
-const user2 = new User("Petr", "petro", "2001-09-12");
-console.log(user2);
-
-Object.defineProperty(user, 'age', {
-    enumerable: true,
-    configurable: true,
-    // writable: true, // без get/set
-    // value: 0, // без get/set
-    get: function () {
-        return age + 'лет'
-    },
-    set: function (a) {
-        if (a > 0 && a < 120) {
-            age = a;
-        } else {
-            throw new Error('Неверное значение');
-        }
-    },
-});
-
-function Contact (name, age, phone, email) {
-    this.name = name;
-    this.age = age;
-    this.phone = phone;
-    this.email = email;
-
-    this.getInfo = () => {
-        return `Name: ${this.name}, Age: ${this.age}, Phone: ${this.phone}, Email: ${this.email}`;
-    }
-
-    this.toString = () => {
-        return `Name: ${this.name}. Age: ${this.age}. Phone: ${this.phone}. Email: ${this.email}. Id: ${this.id}.`
-    }
-}
-
-function Contacts () {
-    this.contacts = [];
-    let id = 0;
+/**
+ * Notes - одна заметка
+ * NoteController
+ * NoteUI
+ */
+/**
+ * @param {{
+ * title: string,
+ * content: string
+ * }} data
+ */
+function Notes(data) { //{title: '', content: ''}
+    if (data.title.length > 0 || data.content.length > 0) this.data = data;
 
     /**
-     * @param {string} name 
-     * @param {number} age 
-     * @param {string} phone 
-     * @param {string} email 
+     * @param {{
+     * title?: string,
+     * content?: string,
+     * }} data 
      */
-    this.add = (name, age, phone, email) => {
-        if (age < 0 || age > 120) return;
-        if (name.length === 0) return;
-        if (phone.length === 0) return;
-        if (email.length === 0) return;
+    this.edit = (data) => {
+        this.data = {
+            ...this.data,
+            ...data,
+        };
 
-        let contact = new Contact(name, age, phone, email);
-        contact.id = id;
-        id++;
-        this.contacts.push(contact);
-    }
+        /**
+         * {
+         * title: 'title 1',
+         * content: 'content 1'
+         * }
+         * 
+         * {
+         * content: 'new content 1'
+         * }
+         */
 
-    this.getContacts = () => {
-        return this.contacts;
-    }
-
-    this.getContact = (id) => {
-        let contact = this.contacts.find((item) => item.id === id);
-        return contact.getInfo();
-    }
-
-    this.removeContact = (id) => {
-        this.contacts = this.contacts.filter((item) => item.id !== id);
+        // Object.assign(this.data, data);
     }
 }
 
-const contactNote = new Contacts();
-contactNote.add('Alex1', 22, '236452374', 'sdgfjds@mail.com');
-contactNote.add('Alex2', 12, '236452374', 'sdgfjds@mail.com');
-contactNote.add('Alex3', 32, '236452374', 'sdgfjds@mail.com');
-contactNote.add('Alex4', 42, '236452374', 'sdgfjds@mail.com');
-contactNote.add('Alex5', 52, '236452374', 'sdgfjds@mail.com');
-contactNote.add('Alex6', 62, '236452374', 'sdgfjds@mail.com');
-contactNote.add('Alex7', 72, '236452374', 'sdgfjds@mail.com');
+// let note = new Notes({title: 'title 1', content: 'content 1'});
+
+function NoteController () {
+    this.notes = [];
+    this.id = 0;
+
+    /**
+     * @param {{
+     * title: string,
+     * content: string,
+     * }} data 
+     */
+    this.add = (data) => {
+        let note = new Notes(data);
+        note.id = this.id++;
+        this.notes.push(note);
+    }
+}
+
+
+NoteController.prototype.remove = function(id) {
+    this.notes = this.notes.filter(item => item.id !== id);
+}
+
+NoteController.prototype.edit = function (id, data) {
+    let note = this.notes.find(item => item.id === id);
+    note.edit(data);   
+}
+
+// let notes = new NoteController();
+
+// notes.add({title: 'title 1', content: 'content 1'});
+// notes.add({title: 'title 2', content: 'content 2'});
+// notes.add({title: 'title 3', content: 'content 3'});
+// notes.add({title: 'title 4', content: 'content 4'});
+// notes.add({title: 'title 5', content: 'content 5'});
+// notes.add({title: 'title 6', content: 'content 6'});
+// notes.add({title: 'title 7', content: 'content 7'});
+// notes.add({title: 'title 8', content: 'content 8'});
+// notes.add({title: 'title 9', content: 'content 9'});
+
+const NoteUI = function (selector) {
+    NoteController.apply(this, arguments);
+    this.root = document.querySelector(selector);
+    this.noteContainer = document.createElement('div');
+    let self = this;
+    /**
+     * 
+     * @param {HTMLElement} elem 
+     * @param {string} eN 
+     * @param {any} func 
+     */
+    this.on = (elem, eN, func) => {
+        elem.addEventListener(eN, func);
+    }
+
+    function onAdd (event) {
+        event.preventDefault();
+        let data = {
+            title: self.title.value || '[Пустой заголовок]',
+            content: self.content.value,
+        }
+        self.add(data);
+        self.title.value = '';
+        self.content.value = '';
+        console.log(self.notes);
+        self.render();
+    }
+
+    this.init = () => {
+        let form = document.createElement('form');
+        this.title = document.createElement('input');
+        this.title.setAttribute('type', 'text');
+        this.content = document.createElement('textarea');
+        this.addBtn = document.createElement('button');
+        this.addBtn.setAttribute('type', 'submit');
+        this.addBtn.innerText = 'Add note';
+
+        this.on(form, 'submit', onAdd);
+
+        form.append(this.title, this.content, this.addBtn);
+        this.root.append(form, this.noteContainer);
+    }
+
+    this.render = () => {
+        this.noteContainer.innerHTML = '';
+        this.notes.forEach(note => {
+            let item = document.createElement('div');
+            let title = document.createElement('h2');
+            let content = document.createElement('p');
+
+            item.classList.add('note__item');
+            title.classList.add('note__title');
+            content.classList.add('note__content');
+
+            title.innerText = note.data.title;
+            content.innerText = note.data.content;
+
+            item.addEventListener('contextmenu', (e) => {
+                e.preventDefault();
+                this.remove(note.id);
+                this.render();
+            });
+
+            item.append(title, content);
+            this.noteContainer.append(item);
+        });
+    }
+}
+
+NoteUI.prototype = Object.create(NoteController.prototype);
+
+new NoteUI('#root').init();
 
 /**
  * map
