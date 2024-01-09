3947f2bb528b6f84108c99e8bb31784d0886d4f6
Evgenii <chico7496@gmail.com>
2024-01-09 21:41:39 +0300

    cw 20

--- src/components/Main.js
+++ src/components/Main.js
@@ -9,11 +9,26 @@ export default class Main {
         let hash = window.location.hash.slice(1);
         if (!hash) hash = 'Home';
         // console.log(hash);
+        /**
+         * @type {null | number}
+         */
+        let id = null;
+        let item = null;
 
+        if (hash.indexOf('_') !== -1){
+            let index = hash.indexOf('_');
+            id = +hash.substring(index).slice(1);
+            hash = hash.slice(0, index);
+        }
         
         const page = await import(`../pajes/${hash}.js`);
-        console.log(page);
-        let item = new page.default().init();
+        // console.log(page);
+        if (!id){
+            item = new page.default().init();
+        } else {
+            item = new page.default(id).init();
+        }
+        
         this.elem.append(item);
     }
 
