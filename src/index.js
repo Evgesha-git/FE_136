78ef5fc771c280d765ccde092235b740578a668e
Evgenii <chico7496@gmail.com>
2024-01-19 21:42:54 +0300

    ts

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
-import './css/style.scss';
+import "./css/style.scss";
+import tsTest from "./main";
 
-const root = document.getElementById('root');
-root.append (
-    new Header().init(),
-    new Main().init(),
-    new Footer().init()
-);
+const root = document.getElementById("root");
+root.append(new Header().init(), new Main().init(), new Footer().init());
 
-console.log('MD5: 2345jk2345');
+tsTest();
+
+console.log("MD5: 2345jk2345");
+
+let div = document.querySelector(".root");
+
+console.log(div?.classList);
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const mode = process.env.MODE_ENV || 'development';
 
@@ -16,20 +18,45 @@ module.exports = {
         path: path.resolve(__dirname, "dist"),
         filename: "[name].[contenthash].js",
     },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js']
+    },
     plugins: [
         new HtmlWebpackPlugin({
             filename: 'index.html',
             template: path.resolve(__dirname, 'src', 'template/index.html')
         }),
         new CleanWebpackPlugin(),
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: '**/*',
+                    context: path.resolve(__dirname, 'src', 'css'),
+                    to: './css',
+                },
+            ]
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'style-[hash].css',
+        }),
     ],
     module: {
         rules: [
             {
                 test: /.(c|sa|sc)ss$/i,
-                use: ['style-loader', 'css-loader', 'sass-loader']
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {},
+                    }, 
+                    'css-loader', 
+                    'sass-loader'
+                ]
             },
-
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+            }
         ]
     }
 };
